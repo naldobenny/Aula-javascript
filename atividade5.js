f72268af205a5712af1ac953899435d48f3855b4
Lucas Campagnoli <lucas.campagnoli@escola.pr.gov.br>
2025-05-23 09:07:59 -0300

    terminando as atividades

---
+++ atividade5.js
@@ -0,0 +1,8 @@
+function alternarVisibilidade(){
+    let caixa = document.getElementById("caixa");
+    if(caixa.stylr.display==="none"){
+        caixa.style.display="block";
+    }else{
+        caixa.style.display = "none";
+    }
+    }
